e362e9ae30c84332c0f17b75ef8dd740617dc0f8
Beth Day <bethmarsh1018@gmail.com>
2023-06-18 15:46:54 -0400

    Re-added bootstrap dependency for layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Info from "./components/Info";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="main-container">
